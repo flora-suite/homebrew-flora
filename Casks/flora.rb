0c32f14f7615016934989680c726e96f8df656e4
vincentdji <168293444+vincentdji@users.noreply.github.com>
2024-11-15 01:31:36 +0800

    Update flora.rb

--- Casks/flora.rb
+++ Casks/flora.rb
@@ -1,8 +1,8 @@
 cask "flora" do
-  version "0.2.0"
-  sha256 "c440010c5077d2870ac56fcc83656b4502a33721a475091495052fa7df22dc46"
+  version "0.0.2"
+  sha256 "a00dbafe215f995c6e47b41b2c57268208fbf61b0def784f5daf596d0ca6da89"
 
-  url "https://github.com/flora-suite/flora/releases/download/v0.2.0/flora-#{version}-mac-universal.dmg"
+  url "https://github.com/flora-suite/flora/releases/download/v#{version}/flora-#{version}-mac-universal.dmg"
   name "Flora"
   desc "Flora is an integrated visualization and diagnosis tool for robotics, available in your browser or macOS"
   homepage "https://flora.fan"
